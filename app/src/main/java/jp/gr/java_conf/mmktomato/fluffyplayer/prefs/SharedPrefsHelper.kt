e04124a79251dbeadcfdb13c385175d88530e3cf
mmktomato <mkt.motohashi@gmail.com>
2017-12-07 18:00:29 +0900

    Refactoring: add FileBrowseActivity's presenter

--- app/src/main/java/jp/gr/java_conf/mmktomato/fluffyplayer/prefs/SharedPrefsHelper.kt
+++ app/src/main/java/jp/gr/java_conf/mmktomato/fluffyplayer/prefs/SharedPrefsHelper.kt
@@ -6,65 +6,63 @@ import android.content.SharedPreferences
 /**
  * An helper of SharedPreferences.
  */
-internal object SharedPrefsHelper {
+internal interface SharedPrefsHelper {
     /**
-     * The preference name.
+     * an android context.
      */
-    private val PREF_NAME = "mmktomato-fluffy-player"
-
-    /**
-     * The preference mode.
-     */
-    private val PREF_MODE = Context.MODE_PRIVATE
+    val context: Context
 
     /**
-     * The key for Dropbox access token.
+     * a Dropbox access token.
      */
-    private val KEY_DBX_ACCESS_TOKEN = "dbx-access-token"
-
-    /**
-     * Returns a SharedPreferences.
-     *
-     * @param ctx An android context.
-     */
-    private fun getSharedPrefs(ctx: Context): SharedPreferences {
-        return ctx.getSharedPreferences(PREF_NAME, PREF_MODE)
-    }
-
-    /**
-     * Returns or sets a string value.
-     *
-     * @param ctx An android context.
-     * @param key The key of SharedPreferences.
-     * @param defaultValue The default value of SharedPreferences.
-     * @param value A value to set to SharedPreferences.
-     */
-    private fun getOrPutString(ctx: Context, key: String, defaultValue: String, value: String?): String {
-        val prefs = getSharedPrefs(ctx)
-
-        if (value != null) {
-            prefs.edit().putString(key, value).apply()
-        }
-
-        return getSharedPrefs(ctx).getString(key, defaultValue)
-    }
-
-    /**
-     * Returns or sets Dropbox access token.
-     *
-     * @param ctx An android context.
-     * @param value A value to set to SharedPreferences.
-     */
-    internal fun dbxAccessToken(ctx: Context, value: String? = null): String {
-        return getOrPutString(ctx, KEY_DBX_ACCESS_TOKEN, "", value)
-    }
+    var dbxAccessToken: String
 
     /**
      * Removes Dropbox access token.
-     *
-     * @param ctx An android context.
      */
-    internal fun removeDbxAccessToken(ctx: Context) {
-        getSharedPrefs(ctx).edit().remove(KEY_DBX_ACCESS_TOKEN).apply()
+    fun removeDbxAccessToken()
+}
+
+/**
+ * An implementation of SharedPrefsHelper.
+ *
+ * @param context An android context.
+ */
+internal class SharedPrefsHelperImpl(override val context: Context) : SharedPrefsHelper {
+    companion object {
+        /**
+         * The preference name.
+         */
+        private val PREF_NAME = "mmktomato-fluffy-player"
+
+        /**
+         * The preference mode.
+         */
+        private val PREF_MODE = Context.MODE_PRIVATE
+
+        /**
+         * The key for Dropbox access token.
+         */
+        private val KEY_DBX_ACCESS_TOKEN = "dbx-access-token"
+    }
+
+    /**
+     * Returns a SharedPreferences.
+     */
+    private val sharedPrefs: SharedPreferences
+        get() = context.getSharedPreferences(PREF_NAME, PREF_MODE)
+
+    /**
+     * Returns or sets Dropbox access token.
+     */
+    override var dbxAccessToken: String
+        get() = sharedPrefs.getString(KEY_DBX_ACCESS_TOKEN, "")
+        set(value) = sharedPrefs.edit().putString(KEY_DBX_ACCESS_TOKEN, value).apply()
+
+    /**
+     * Removes Dropbox access token.
+     */
+    override fun removeDbxAccessToken() {
+        sharedPrefs.edit().remove(KEY_DBX_ACCESS_TOKEN).apply()
     }
 }
