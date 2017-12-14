77e2753adaeae496f26ab5a85e1a001add7db47d
mmktomato <mkt.motohashi@gmail.com>
2017-12-14 16:01:40 +0900

    avoid moving to FileBrowseActivity without access token.

--- app/src/main/java/jp/gr/java_conf/mmktomato/fluffyplayer/MainActivity.kt
+++ app/src/main/java/jp/gr/java_conf/mmktomato/fluffyplayer/MainActivity.kt
@@ -5,6 +5,9 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import android.widget.Toast
+import jp.gr.java_conf.mmktomato.fluffyplayer.dropbox.DbxProxy
+import jp.gr.java_conf.mmktomato.fluffyplayer.prefs.SharedPrefsHelperImpl
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,17 +15,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // TODO: !!!!! refactor this !!!!!
+
         val listContents = listOf("Browse Dropbox files", "Settings")
         val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, listContents)
+        val sharedProfs = SharedPrefsHelperImpl(this)
+        val dbxProxy = DbxProxy(sharedProfs)
 
         val listView = findViewById<ListView>(R.id.mainListView)
         listView.adapter = adapter
         listView.setOnItemClickListener { parent, view, position, id ->
             when (position) {
+                // Browse Dropbox files
                 0 -> {
-                    val intent = Intent(this, FileBrowseActivity::class.java)
-                    startActivity(intent)
+                    if (dbxProxy.isAuthenticated()) {
+                        val intent = Intent(this, FileBrowseActivity::class.java)
+                        startActivity(intent)
+                    }
+                    else {
+                        Toast.makeText(this, "Please connect to Dropbox. See 'Settings > Dropbox'", Toast.LENGTH_LONG).show()
+                    }
                 }
+                // Settings
                 1 -> {
                     val intent = Intent(this, SettingsActivity::class.java)
                     startActivity(intent)
