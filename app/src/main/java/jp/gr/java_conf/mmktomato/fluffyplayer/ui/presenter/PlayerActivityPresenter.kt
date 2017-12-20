7093d9f8a53646e5d17742818abc8d04ffadc6f9
mmktomato <mkt.motohashi@gmail.com>
2017-12-20 17:38:12 +0900

    show music title to notification.

--- app/src/main/java/jp/gr/java_conf/mmktomato/fluffyplayer/ui/presenter/PlayerActivityPresenter.kt
+++ app/src/main/java/jp/gr/java_conf/mmktomato/fluffyplayer/ui/presenter/PlayerActivityPresenter.kt
@@ -1,5 +1,6 @@
 package jp.gr.java_conf.mmktomato.fluffyplayer.ui.presenter
 
+import android.app.NotificationManager
 import android.content.Intent
 import android.content.res.Resources
 import android.graphics.drawable.Drawable
@@ -12,7 +13,9 @@ import jp.gr.java_conf.mmktomato.fluffyplayer.dropbox.DbxProxy
 import jp.gr.java_conf.mmktomato.fluffyplayer.entity.MusicMetadata
 import jp.gr.java_conf.mmktomato.fluffyplayer.player.PlayerService
 import jp.gr.java_conf.mmktomato.fluffyplayer.player.PlayerServiceState
+import jp.gr.java_conf.mmktomato.fluffyplayer.prefs.SharedPrefsHelper
 import jp.gr.java_conf.mmktomato.fluffyplayer.ui.viewmodel.PlayerActivityViewModel
+import jp.gr.java_conf.mmktomato.fluffyplayer.usecase.NotificationUseCase
 import kotlinx.coroutines.experimental.*
 import java.io.ByteArrayInputStream
 
@@ -20,6 +23,11 @@ import java.io.ByteArrayInputStream
  * A presenter of PlayerActivity.
  */
 internal interface PlayerActivityPresenter {
+    /**
+     * the NotificationUseCase.
+     */
+    val notificationUseCase: NotificationUseCase
+
     /**
      * the view model of this activity.
      */
@@ -44,6 +52,11 @@ internal interface PlayerActivityPresenter {
      * a callback to bind a service.
      */
     val unbindService: () -> Unit
+
+    /**
+     * a callback to get string resources.
+     */
+    val getString: (Int) -> String
 
     fun onCreate()
 
@@ -99,6 +112,7 @@ internal interface PlayerActivityPresenter {
         preparePlayerJob.join()
 
         setMusicMetadata(musicMetadata)
+        updateNotification(musicMetadata)
         if (!svcState.binder.isPlaying) {
             svcState.binder.start()
         }
@@ -140,11 +154,18 @@ internal interface PlayerActivityPresenter {
      */
     fun resetUI() {
         setMusicMetadata(MusicMetadata(
-                title = "(Loading...)",
+                title = getString(R.string.now_loading_text),
                 artwork = noArtworkImage))
 
         viewModel.isPlaying.set(false)
     }
+
+    /**
+     * Updates now playing notification.
+     *
+     * @param metadata a music metadata.
+     */
+    fun updateNotification(metadata: MusicMetadata)
 
     /**
      * Returns the empty album artwork.
@@ -155,6 +176,7 @@ internal interface PlayerActivityPresenter {
 /**
  * An implementation of PlayerActivityPresenter.
  *
+ * @param sharedPrefs the SharedPrefsHelper.
  * @param dbxProxy the Dropbox API Proxy.
  * @param viewModel the view model.
  * @param dbxMetadata the Dropbox's node metadata.
@@ -162,10 +184,13 @@ internal interface PlayerActivityPresenter {
  * @param startService the callback to start a service.
  * @param bindService the callback to bind a service.
  * @param unbindService the callback to unbind a service.
+ * @param getString the callback to get string resources.
+ * @param notificationManager the NotificationManager.
  * @param playButton the play button.
  * @param resources android's resource.
  */
 internal class PlayerActivityPresenterImpl(
+        private val sharedPrefs: SharedPrefsHelper,
         private val dbxProxy: DbxProxy,
         override val viewModel: PlayerActivityViewModel,
         override val dbxMetadata: DbxNodeMetadata,
@@ -173,8 +198,15 @@ internal class PlayerActivityPresenterImpl(
         private val startService: (Intent) -> Unit,
         private val bindService: (Intent) -> Unit,
         override val unbindService: () -> Unit,
+        override val getString: (Int) -> String,
+        private val notificationManager: NotificationManager,
         private val playButton: Button,
         private val resources: Resources) : PlayerActivityPresenter {
+
+    /**
+     * the NotificationUseCase.
+     */
+    override val notificationUseCase = NotificationUseCase()
 
     /**
      * the player service state.
@@ -224,6 +256,18 @@ internal class PlayerActivityPresenterImpl(
      */
     override fun getMusicUri(): Deferred<String> {
         return dbxProxy.getTemporaryLink(dbxMetadata.path)
+    }
+
+    /**
+     * Updates now playing notification.
+     *
+     * @param metadata a music metadata.
+     */
+    override fun updateNotification(metadata: MusicMetadata) {
+        val notification = notificationUseCase.createNowPlayingNotification(
+                sharedPrefs.context, dbxMetadata, metadata.title ?: getString(R.string.unknown_music_title))
+
+        notificationManager.notify(PlayerService.NOTIFICATION_ID, notification)
     }
 
     /**
