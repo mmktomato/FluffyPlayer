8945fcbae2423c9eda6bbb8ec27bbbe6a74a13bf
mmktomato <mkt.motohashi@gmail.com>
2018-01-26 16:17:42 +0900

    Fix: AppDatabase is not singleton

--- app/src/main/java/jp/gr/java_conf/mmktomato/fluffyplayer/db/AppDatabase.kt
+++ app/src/main/java/jp/gr/java_conf/mmktomato/fluffyplayer/db/AppDatabase.kt
@@ -1,7 +1,9 @@
 package jp.gr.java_conf.mmktomato.fluffyplayer.db
 
 import android.arch.persistence.room.Database
+import android.arch.persistence.room.Room
 import android.arch.persistence.room.RoomDatabase
+import android.content.Context
 import jp.gr.java_conf.mmktomato.fluffyplayer.db.model.PlaylistItem
 import jp.gr.java_conf.mmktomato.fluffyplayer.db.model.PlaylistItemDao
 
@@ -11,4 +13,19 @@ abstract class AppDatabase : RoomDatabase() {
      * Returns PlaylistDao.
      */
     abstract val playlistDao: PlaylistItemDao
+
+    //companion object Factory {  This causes `java.lang.NoSuchFieldError` because `AppDatabase` class is abstract.
+    object Factory {
+        private lateinit var instance: AppDatabase
+
+        /**
+         * Factory of AppDatabase.
+         */
+        fun create(ctx: Context): AppDatabase {
+            if (!::instance.isInitialized) {
+                instance = Room.databaseBuilder(ctx, AppDatabase::class.java, "fluffy.db").build()
+            }
+            return instance
+        }
+    }
 }
